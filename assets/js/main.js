df0ce81f5504b678f6d2f697aaa0f0bb5808fd8d
Riccardo Murru <riccardo.mr@gmail.com>
2020-03-30 17:52:46 +0200

    add. js email validation with simple form, buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,31 +11,59 @@
 var accounts = ['michele@boolean.careers', 'fabio@boolean.careers', 'roberto@boolean.careers'];
 var userMail;
 var matchedAccount = false;
+var btnEnter;
+var btnCancel;
+var error = document.getElementById('error');
+var welcome = document.getElementById('welcome');
+var messageBox = document.getElementById('message-box');
 
 // Var gioco dadi
 var numberPlayer;
 var numberNpc;
 
+// Bottoni invio e annulla
+btnEnter = document.getElementById('btnEnter');
+btnCancel = document.getElementById('btnCancel');
 
-// 1. Chiedere all'utente di inserire la mail
+btnEnter.addEventListener( 'click',
+    function() {
+        // 1. Acquisire email dall'utente
+        userMail = document.getElementById('email').value;
+        console.log(userMail);
 
-userMail = prompt('Inserisci il tuo indirizzo email');
-
-// 2. Verificare che sia nell'array
-
-for (var i = 0; i < accounts.length; i++) {
-    if ( userMail === accounts[i]) {
-        matchedAccount = true;
-
-        // 3. Stampare messaggio di conferma
-        console.log('Benvenuto', accounts[i]); 
+        // 2. Verificare che sia nell'array
+        for (var i = 0; i < accounts.length; i++) {
+            if ( userMail === accounts[i]) {
+                matchedAccount = true;
+        
+                // 3. Stampare messaggio di conferma
+                messageBox.className = 'visible';
+                message.className = 'visible';
+                document.getElementById('message').innerHTML = 'Benvenuto ' + accounts[i] + '!'; 
+            }
+        }
+        
+        // Stampa messaggio di errore
+        if (matchedAccount === false) {
+            
+            messageBox.className = 'visible';
+            message.className = 'visible';
+            console.log('errore');
+            
+            document.getElementById('message').innerHTML = 'Errore di autenticazione.'; 
+        }
     }
-}
+);
 
-// Stampa messaggio di errore
-if (matchedAccount === false) {
-    console.log('Errore di autenticazione');   
-}
+btnCancel.addEventListener( 'click',
+    function() {
+        document.getElementById('email').value ='';
+        
+        //reset condizioni
+        matchedAccount = false;
+        messageBox.className = 'hidden';
+    }
+);
 
 // 4. Generare due numeri casuali da 1 a 6
 
